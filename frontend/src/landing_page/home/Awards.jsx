a3356a140d4ac7b8a94b7a766f404fadc1d395aa
Aniket-W24 <aniket.soni.w24@gmail.com>
2024-06-21 19:38:24 +0530

    Built Awards & Hero

--- frontend/src/landing_page/home/Awards.jsx
+++ frontend/src/landing_page/home/Awards.jsx
@@ -1,7 +1,32 @@
 export default function Awards(){
     return (
-        <>
-            <h1>Awards</h1>
-        </>
+        <div className="container mt-5 mb-5">
+            <div className="row">
+                <div className="col-6 p-5">
+                    <img src="media/images/largest-broker.svg"></img>
+                </div>
+                <div className="col-6 p-5">
+                    <h1>Largest stock broker in India</h1>
+                    <p className="mb-4 mt-3">1.5+ Crore Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+                    <div className="row">
+                        <div className="col-6">
+                            <ul>
+                                <li>Futures and Options</li>
+                                <li>Commodity derivatives</li>
+                                <li>Currency derivatives</li>
+                            </ul>
+                        </div>
+                        <div className="col-6">
+                            <ul>
+                                <li>Stocks & IPOs</li>
+                                <li>Direct mutual funds</li>
+                                <li>Bonds and Govt. Securities</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <img src="media/images/press-logos.png" alt="press-logo" style={{width: "90%"}}/>
+                </div>
+            </div>
+        </div>
     )
 }
--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,7 +1,14 @@
 export default function Hero(){
     return (
         <>
-            <h1>Hero<i class="fa-solid fa-house"></i></h1>
+            <div className="container p-5 mb-5">
+                <div className="row text-center">
+                    <img src="media/images/landing.png" alt="hero.png"/>
+                    <h1 className="mt-5">Invest in everything</h1>
+                    <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
+                    <button className="p-2 btn btn-primary fs-5" style={{width: "25%", margin: "0 auto"}}>Sign up now</button>
+                </div>
+            </div>
         </>
     )
 }
