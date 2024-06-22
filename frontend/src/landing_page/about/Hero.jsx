2f403b27892c8188c241b2bf7b08734dc3bdb6dc
Aniket-W24 <aniket.soni.w24@gmail.com>
2024-06-22 15:33:59 +0530

    About page Done

--- frontend/src/landing_page/about/Hero.jsx
+++ frontend/src/landing_page/about/Hero.jsx
@@ -1,7 +1,28 @@
 export default function Hero(){
     return (
-        <>
-            <h1>Hero</h1>
-        </>
+        <div className="container">
+            <div className="row mt-5 mb-5 p-5 text-center">
+                <h1 className="fs-2 fw-normal" style={{lineHeight: "3rem"}}>We pioneered the discount broking model in India.
+                <br></br>Now, we are breaking ground with our technology.</h1>
+            </div>
+
+            <div className="row mt-5 mb-5 p-5 text-center border-top">
+                <div className="col-6 text-muted p-5" style={{lineHeight: "2rem", fontSize: "1.1rem"}}>
+                <p>We kick-started operations on the 15th of August, 2010 with the goal of breaking all barriers that traders and investors face in India in terms of cost, support, and technology. We named the company Zerodha, a combination of Zero and "Rodha", the Sanskrit word for barrier.</p>
+
+                <p>Today, our disruptive pricing models and in-house technology have made us the biggest stock broker in India.</p>
+
+                <p>Over 1+ Crore clients place millions of orders every day through our powerful ecosystem of investment platforms, contributing over 15% of all Indian retail trading volumes.</p>
+                </div>
+
+                <div className="col-6 text-muted fs-5 p-5" style={{lineHeight: "2rem", fontSize: "1.3rem"}}>
+                <p>In addition, we run a number of popular open online educational and community initiatives to empower retail traders and investors.</p>
+
+                <p><a href="" style={{textDecoration: "none"}}>Rainmatter</a>, our fintech fund and incubator, has invested in several fintech startups with the goal of growing the Indian capital markets.</p>
+
+                <p>And yet, we are always up to something new every day. Catch up on the latest updates on our <a href="" style={{textDecoration: "none"}}>blog</a> or see what the media is <a href="" style={{textDecoration: "none"}}>saying about us.</a></p>
+                </div>
+            </div>
+        </div>
     )
 }
--- frontend/src/landing_page/about/Team.jsx
+++ frontend/src/landing_page/about/Team.jsx
@@ -1,7 +1,27 @@
 export default function Team(){
     return (
-        <>
-            <h1>Team</h1>
-        </>
+        <div className="container">
+            <div className="row mb-5 text-center">
+                <h1 className="fw-normal">People</h1>
+            </div>
+
+            <div className="row">
+                <div className="col-6 text-muted text-center p-5" >
+                    <img src="media/images/myImg.JPEG" style={{width: "60%", height: "19rem", borderRadius: "50%"}}/>
+                    <h4 className="mt-4">Aniket</h4>
+                    <h6>Founder, CEO</h6>
+                </div>
+
+                <div className="col-6 text-muted fs-6 py-5" style={{lineHeight: "2rem", fontSize: "1.3rem"}}>
+                <p>Aniket bootstrapped and founded StockSage in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, StockSage has changed the landscape of the Indian broking industry.</p>
+
+                <p>He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).</p>
+
+                <p>Playing Video Games is his zen.</p>
+
+                <p>Connect on <a href="" style={{textDecoration: "none"}}>Homepage</a> / <a href="" style={{textDecoration: "none"}}>TradingQnA</a> / <a href="https://x.com/sonianiket179" target="_blank" style={{textDecoration: "none"}}>Twitter</a> </p>
+                </div>
+            </div>
+        </div>
     )
 }
