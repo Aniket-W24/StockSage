94b113847977a3e2f663ba49827d4dc00a51987f
Aniket-W24 <aniket.soni.w24@gmail.com>
2024-06-24 17:40:01 +0530

    Setup Dashboard

---
+++ dashboard/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from './App.jsx'
+import Home from "./components/Home.jsx";
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
